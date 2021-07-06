bab49b27f089eb975159f2e32bb3c1bb54e71166
ummesalmaali <ummes3527@gmail.com>
2021-07-06 22:21:41 +0600

    mobile-meny-added

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,3 +8,31 @@ new Waypoint( {
     },
     offset: 'bottom-in-view',
 } )
+// for sticky navigation
+$(document).ready(function () {
+    //dESIGNING STICKY MENU//
+    $(".js--services-section").waypoint(function (direction) {
+        if (direction == "down") {
+            $("nav").addClass("sticky");
+       
+        } else {
+            $("nav").removeClass("sticky");
+           
+        }
+    });
+
+
+
+//    smooyh scroll for ie/edge/safari
+
+
+
+
+});
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
